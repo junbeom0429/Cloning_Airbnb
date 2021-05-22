ca8f80159ab711f06638b7a245f3feca98c1b2f1
Junbeom Lee <junbeom0429@gmail.com>
2021-05-23 01:19:27 +0900

    inital commit

---
+++ airbnb/Configuration/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  airbnb
+//
+//  Created by JB on 2021/05/22.
+//
+
+import Foundation
+
+struct Constant {
+    static let googleOAuth = "186497991722-ts2ubj7gaf851vfv8b3vcnqjq0g5p5mf.apps.googleusercontent.com"
+    
+}
