e3623f92d3b8608a2f8407b2eb90c0a4d980d4a7
junbeom0429 <junbeom0429@gmail.com>
2021-05-25 03:33:36 +0900

    improve login view

--- airbnb/Sources/Login/LoginVC.swift
+++ airbnb/Sources/Login/LoginVC.swift
@@ -12,14 +12,20 @@ class LoginVC: BaseViewController {
     override func viewDidLoad() {
         LoginAPIConfig()
         signUpContainerConfig()
+        emailContainerConfig()
     }
     // MARK: - Header Container
     
+
     // MARK: - Sign Up Container
     // 변수
     var constraints: [NSLayoutConstraint] = []
+    var PNConstraints:  [NSLayoutConstraint] = [
+        
+    ]
     var isShowed = false
     var showedHighlightView = UIView()
+    var ismoved = false
     
     // 아웃렛
     @IBOutlet weak var signUpContainer: UIView!
@@ -29,6 +35,13 @@ class LoginVC: BaseViewController {
     @IBOutlet weak var highlightView2: UIView!
     @IBOutlet weak var ContinueBtn: UIButton!
     @IBOutlet weak var signUpStackView: UIStackView!
+    @IBOutlet weak var phoneNumberOriginLoc: UIView!
+    @IBOutlet weak var phoneNumberTargetLoc: UIView!
+    @IBOutlet weak var originLocTop: NSLayoutConstraint!
+    @IBOutlet weak var originLocLeading: NSLayoutConstraint!
+    @IBOutlet weak var originLocHeight: NSLayoutConstraint!
+    @IBOutlet weak var originLocWidth: NSLayoutConstraint!
+    @IBOutlet weak var phoneNumTextField: UITextField!
     
     // 액션
     @IBAction func touchCountryBtn(_ sender: Any) {
@@ -39,14 +52,34 @@ class LoginVC: BaseViewController {
     @IBAction func touchPhoneNumberBtn(_ sender: Any) {
         showHighlightView(target: phoneNumberView, highlight: highlightView2)
         moveHighlightView(target: phoneNumberView)
+        movePN(target: phoneNumberTargetLoc)
+        phoneNumTextField.becomeFirstResponder()
     }
-    
+    func movePN(target: UIView) {
+        if ismoved == false {
+            phoneNumberView.removeConstraint(originLocLeading)
+            phoneNumberView.removeConstraint(originLocTop)
+            phoneNumberOriginLoc.removeConstraint(originLocHeight)
+            phoneNumberOriginLoc.removeConstraint(originLocWidth)
+            phoneNumberOriginLoc.translatesAutoresizingMaskIntoConstraints = false
+            PNConstraints = [
+                phoneNumberOriginLoc.leadingAnchor.constraint(equalTo: target.leadingAnchor),
+                phoneNumberOriginLoc.trailingAnchor.constraint(equalTo: target.trailingAnchor),
+                phoneNumberOriginLoc.topAnchor.constraint(equalTo: target.topAnchor),
+                phoneNumberOriginLoc.bottomAnchor.constraint(equalTo: target.bottomAnchor)
+            ]
+            NSLayoutConstraint.activate(PNConstraints)
+            UIView.animate(withDuration: 0.2) {
+                self.view.layoutIfNeeded()
+            }
+            ismoved = true
+        }
+    }
     // 첫 터치시 뷰 보이기
     func showHighlightView(target: UIView, highlight: UIView) {
         if isShowed == false {
             UIView.animate(withDuration: 0.15) {
                 highlight.layer.borderWidth = 2
-//                highlight.layer.cornerRadius = 10
                 highlight.layer.borderColor = UIColor.black.cgColor
             }
             isShowed = true
@@ -71,15 +104,7 @@ class LoginVC: BaseViewController {
     }
     func signUpContainerConfig() {
         ContinueBtn.layer.cornerRadius = 10
-        
-        
-//        countryView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-//        countryView.layer.cornerRadius = 10
-//        countryView.layer.borderWidth = 1
-//        countryView.layer.borderColor = UIColor.darkGray.cgColor
-//        countryView.layer.addBorder([.top,.left,.right], color: .darkGray, width: 1)
-        
-        
+              
         phoneNumberView.layer.cornerRadius = 10
         phoneNumberView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         phoneNumberView.layer.borderWidth = 1
@@ -90,6 +115,74 @@ class LoginVC: BaseViewController {
         
     }
     
+    // MARK: - Email Container
+    //변수
+    var isEmailHighlighted = false
+    var isTouchedEmail = false
+    var emailConstraints: [NSLayoutConstraint] = []
+    
+    //아웃렛
+    @IBOutlet weak var emailContainer: UIView!
+    @IBOutlet weak var emailView: UIView!
+    @IBOutlet weak var highlightView3: UIView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var emailContinueBtn: UIButton!
+    @IBOutlet weak var emailOriginLoc: UIView!
+    @IBOutlet weak var emailTargetLoc: UIView!
+    
+    @IBOutlet weak var emailHeight: NSLayoutConstraint!
+    @IBOutlet weak var emailWidth: NSLayoutConstraint!
+    @IBOutlet weak var emailTop: NSLayoutConstraint!
+    @IBOutlet weak var emailLeading: NSLayoutConstraint!
+    
+    //액션
+    @IBAction func emailViewAction(_ sender: Any) {
+        moveEmail(target: emailTargetLoc)
+        showEmailHighlight(highlight: highlightView3)
+        emailTextField.becomeFirstResponder()
+    }
+    
+    func moveEmail(target: UIView) {
+        if isTouchedEmail == false {
+            emailView.removeConstraint(emailTop)
+            emailView.removeConstraint(emailLeading)
+            emailOriginLoc.removeConstraint(emailHeight)
+            emailOriginLoc.removeConstraint(emailWidth)
+            emailOriginLoc.translatesAutoresizingMaskIntoConstraints = false
+            emailConstraints = [
+                emailOriginLoc.leadingAnchor.constraint(equalTo: target.leadingAnchor),
+                emailOriginLoc.trailingAnchor.constraint(equalTo: target.trailingAnchor),
+                emailOriginLoc.topAnchor.constraint(equalTo: target.topAnchor),
+                emailOriginLoc.bottomAnchor.constraint(equalTo: target.bottomAnchor)
+            ]
+            NSLayoutConstraint.activate(emailConstraints)
+            UIView.animate(withDuration: 0.2) {
+                self.view.layoutIfNeeded()
+            }
+            isTouchedEmail = true
+        }
+    }
+    
+    func showEmailHighlight(highlight: UIView) {
+        if isEmailHighlighted == false {
+            UIView.animate(withDuration: 0.15) {
+                highlight.layer.borderWidth = 2
+                highlight.layer.borderColor = UIColor.black.cgColor
+            }
+            isEmailHighlighted = true
+        }
+    }
+    
+    func emailContainerConfig() {
+        emailContinueBtn.layer.cornerRadius = 10
+        
+        highlightView3.layer.cornerRadius = 10
+        
+        emailView.layer.cornerRadius = 10
+        emailView.layer.borderWidth = 1
+        emailView.layer.borderColor = UIColor.borderLightGray.cgColor
+    }
+    
     // MARK: - LoginAPI Container
     // 변수
     var isEmail = false
@@ -97,9 +190,7 @@ class LoginVC: BaseViewController {
     
     // 아웃렛
     @IBOutlet weak var loginAPIContainer: UIView!
-    @IBOutlet weak var emailContainer: UIView!
     @IBOutlet weak var LoginAPITopConstraint: NSLayoutConstraint!
-    @IBOutlet weak var emailBtn: UIButton!
     @IBOutlet weak var appleBtn: UIButton!
     @IBOutlet weak var googleBtn: UIButton!
     @IBOutlet weak var facebookBtn: UIButton!
@@ -168,7 +259,6 @@ class LoginVC: BaseViewController {
     }
     func LoginAPIConfig() {
         emailOutlet.layer.cornerRadius = 5
-        emailBtn.layer.cornerRadius = 5
         emailOutlet.layer.borderWidth = 0.5
         emailOutlet.layer.borderColor = UIColor.darkGray.cgColor
         
